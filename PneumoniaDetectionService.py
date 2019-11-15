b3a79acf42ba7e97554017e860c8bb7970ebf963
Karthik Reddy <56941019+KarthikReddy2409@users.noreply.github.com>
2019-11-15 10:12:28 -0500

    Add files via upload

---
+++ PneumoniaDetectionService.py
@@ -0,0 +1,80 @@
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+
+# re-size all the images to this
+IMAGE_SIZE = [224, 224]
+
+train_path = '/Users/karthikreddy/Desktop/PneumoniaDetector/chest_xray/train'
+test_path = '/Users/karthikreddy/Desktop/PneumoniaDetector/chest_xray/test'
+
+# add preprocessing layer to the front of VGG
+vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# don't train existing weights
+for layer in vgg.layers:
+  layer.trainable = False
+  
+   # useful for getting number of classes
+folders = glob('/Users/karthikreddy/Desktop/PneumoniaDetector/chest_xray/train/*')
+
+# our layers - you can add more if you want
+x = Flatten()(vgg.output)
+
+# x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=vgg.input, outputs=prediction)
+
+# view the structure of the model
+model.summary()
+
+# tell the model what cost and optimization method to use
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('/Users/karthikreddy/Desktop/PneumoniaDetector/chest_xray/train',
+                                                 target_size = (224, 224),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('/Users/karthikreddy/Desktop/PneumoniaDetector/chest_xray/test',
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+# fit the model
+r = model.fit_generator(
+  training_set,
+  validation_data=test_set,
+  epochs=5,
+  steps_per_epoch=len(training_set),
+  validation_steps=len(test_set)
+)
+
+import tensorflow as tf
+
+from keras.models import load_model
+
+model.save('model_vgg19.h5')
